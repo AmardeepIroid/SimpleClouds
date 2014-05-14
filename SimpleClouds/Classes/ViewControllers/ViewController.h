1bbff0ee7c71972fb9f3858fb923fde19e23e885
Alexander Matrosov <matrosov.developer@gmail.com>
2014-05-14 16:46:23 +0300

    first commit

---
+++ SimpleClouds/Classes/ViewControllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleClouds
+//
+//  Created by mac on 14.05.14.
+//  Copyright (c) 2014 m-dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SimpleClouds/Classes/ViewControllers/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SimpleClouds
+//
+//  Created by mac on 14.05.14.
+//  Copyright (c) 2014 m-dev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
